0c5eb6a1b19eef8452773c4a5762462b9fbdcc62
PushAc <PushAc@192.168.0.8>
2018-12-06 22:25:08 +0900

    思い出しならやりました

---
+++ dep_web/src/test/DateServlet.java
@@ -0,0 +1,51 @@
+package test;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DateServlet
+ */
+@WebServlet("/DateServlet")
+public class DateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String date = request.getParameter("date");
+		
+		if(date.equals("2018-02-08")) {
+			System.out.println("正解");
+		}else {
+			request.getRequestDispatcher("viewCalender.jsp").forward(request, response);
+		}
+	}
+
+}
